8ff16f4c89f2df8c242aca7032eb09b770977d71
Oleg Ivanov <juklino@yandex.ru>
2021-10-11 16:22:19 +0300

    Добавил проект для учебных задач.

---
+++ Sololearn/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPrice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Discount(totalPrice));
+            Console.ReadLine();
+        }
+
+        static int Discount(int price)
+        {
+            if (price >= 10000)
+                return Convert.ToInt32(price - (0.2 * price));
+            else
+                return price;
+        }
+    }
+}
