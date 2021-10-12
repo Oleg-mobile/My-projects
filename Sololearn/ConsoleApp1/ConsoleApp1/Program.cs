345780b9df618d1164ef035dafcdfbc9d3228552
Oleg Ivanov <juklino@yandex.ru>
2021-10-12 16:32:14 +0300

    Добавил рекурсию.

--- Sololearn/ConsoleApp1/ConsoleApp1/Program.cs
+++ Sololearn/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,18 +10,46 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int totalPrice = Convert.ToInt32(Console.ReadLine());
+            //int totalPrice = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(Discount(totalPrice));
 
-            Console.WriteLine(Discount(totalPrice));
+            //DrawPyramid(6);
+
+            int levels = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Points(levels));
+
             Console.ReadLine();
         }
 
-        static int Discount(int price)
+        static int Discount(int price)  //  чена с учётом скидки
         {
             if (price >= 10000)
                 return Convert.ToInt32(price - (0.2 * price));
             else
                 return price;
+        }
+
+        static void DrawPyramid(int n) // рисует пирамиду
+        {
+            for (int i = 1; i <= n; i++)  // построчно
+            {
+                for (int j = i; j <= n; j++)  // рисует пробелы
+                {
+                    Console.Write("  ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)  // рисует звёздочки
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int Points(int levels)
+        {
+            if (levels == 0)
+                return 0;
+            return levels + Points(levels - 1);
         }
     }
 }
