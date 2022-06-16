59b9cc2859b12e926a32528e5bc1398f6c688325
Oleg Ivanov <juklino@yandex.ru>
2022-06-16 15:52:51 +0300

    Шаблон задачи

---
+++ Reminder/TaskTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder
+{
+    public enum TaskType { Important, Usual, Special }
+    internal class TaskTemplate
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public TaskType Type { get; set; }
+    }
+}
